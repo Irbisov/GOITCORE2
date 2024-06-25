d4c72d1a941acbe08ac9a76ffe48eae21119035e
Ihor Palii <paliiihor69@gmail.com>
2024-06-25 15:13:03 +0300

    Done home work 3

--- goit-algo-hw-03/third.py
+++ goit-algo-hw-03/third.py
@@ -1,3 +1,33 @@
+raw_numbers = [
+    "+380441234567",
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+
+def normalize_phone(phone_number):
+    digit_number = ""  # mobile number without garbage
+    for i in phone_number:
+        if i.isdigit():
+            digit_number += i
+    if len(digit_number) == 10:
+        digit_number = '+38' + digit_number
+    elif len(digit_number) == 12:
+        digit_number = '+' + digit_number
+
+
+    return digit_number
+
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+
 """Третє завдання (не обов'язкове)
 
 
