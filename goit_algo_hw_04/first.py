8010ac6b141bb52ccdd9ac6f1ac59ac4b624b043
Ihor Palii <paliiihor69@gmail.com>
2024-07-07 20:36:42 +0300

    Done home work 2 Task 1 Add try/exept ZeroDivisionError,IndexError

--- goit_algo_hw_04/first.py
+++ goit_algo_hw_04/first.py
@@ -5,11 +5,17 @@ def total_salary(path):
     if os.path.exists(path):
         with open(path) as fh:
             list_unit = fh.readlines()
+        try:
             ls = [int((i.split(','))[1]) for i in list_unit]
-        total = sum(ls)
-        average = int(total / len(ls))
-        return print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-    return print(f'{path} to file is not found')
+        except IndexError:
+            return print('List is out of range.')
+        try:
+            total = sum(ls)
+            average = int(total / len(ls))
+            return print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}.")
+        except ZeroDivisionError:
+            return print('File is empty.')
+    return print(f'{path} to file is not found.')
 
 
 total_salary('first_example.txt')
